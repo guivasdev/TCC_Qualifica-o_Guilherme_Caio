698e56ed29b3b55d05f57950381c1d4026801b56
CaioDiasPereira <101738426+CaioDiasPereira@users.noreply.github.com>
2025-12-08 13:30:32 -0300

    A

--- model/BuscarAtaModel.php
+++ model/BuscarAtaModel.php
@@ -1,11 +1,11 @@
 <?php 
-    require_once __DIR__ . '/../model/classes/Pesquisa.php';
 
     class BuscarAtaModel{
 
         public function buscarAtas(){
+            require_once __DIR__ . '/../model/classes/Pesquisa.php';
             $Busca = new Pesquisa();
-            
+
             return $Busca->Pesquisar();
         }
     }
