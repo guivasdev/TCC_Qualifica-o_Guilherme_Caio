4b8176f911221af07a9cf32d64dfcee5438fbffb
CaioDiasPereira <101738426+CaioDiasPereira@users.noreply.github.com>
2025-12-09 09:53:59 -0300

    dadadsaddadad

--- view/ata/componentes.php
+++ view/ata/componentes.php
@@ -12,9 +12,22 @@ if (!function_exists('inputSelectEInput')) {
         $html .= "<select name='{$name}_id' class='form-control' style='max-width:150px'>";
         $html .= "<option value=''>Selecionar</option>";
 
-        foreach ($opcoes as $op) {
+        
+
+        if ($opcoes['nome'] == "integrante"){
+            
+            foreach ($opcoes as $op) {
+                //$html .= "<option value='{$op['id']}'>{$op['nome']}</option>";
+                $html .= "<input type=checkbox  name={$op['nome']} value='{$op['id']}'>";
+            }
+        } else{
+
+            foreach ($opcoes as $op) {
             $html .= "<option value='{$op['id']}'>{$op['nome']}</option>";
+            }
         }
+
+        $html .= "<option value='{$op['id']}'>{$op['nome']}</option>";
 
         $html .= "</select>";
 
